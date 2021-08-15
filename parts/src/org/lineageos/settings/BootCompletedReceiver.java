98ff08d5d8fe69c20336d5ab7e99b108c4374f86
Pranav Vashi <neobuddy89@gmail.com>
2021-08-16 02:18:33 +0530

    sweet: parts: Nuke doze settings
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -24,7 +24,6 @@ import android.os.SystemProperties;
 import androidx.preference.PreferenceManager;
 
 import org.lineageos.settings.dirac.DiracUtils;
-import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.utils.RefreshRateUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.utils.FileUtils;
@@ -41,9 +40,9 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         // Refresh rate
         RefreshRateUtils.setFPS(RefreshRateUtils.getRefreshRate(context));
 
-        // Doze
+        // Dirac
         DiracUtils.initialize(context);
-        DozeUtils.checkDozeService(context);
+
         // Thermal Profiles
         ThermalUtils.startService(context);
         // DC Dimming
