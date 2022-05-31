4c11f8ec5eb340e5e516af72d636b6ebc8491c8a
Pranav Vashi <neobuddy89@gmail.com>
2022-06-01 02:44:27 +0530

    sweet: Configure bootanimation size and AAPT config
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: If0809a17a4a6dbafe20d4d4aca6cc2edb16253ba

--- device.mk
+++ device.mk
@@ -17,6 +17,14 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
