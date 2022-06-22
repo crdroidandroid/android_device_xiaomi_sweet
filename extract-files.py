4c7336fa496a94ccac6f5f0a9ca0c42e93fe5906
Arian <arian.kulmer@web.de>
2022-06-22 16:31:14 +0200

    sweet: Bring back face detection libs
    Change-Id: I50af944e9398eef352f72f4729999f8d0c556837
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -74,6 +74,10 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libpiex_shim.so'),
     'vendor/lib64/camera/components/com.vidhance.node.eis.so': blob_fixup()
         .call(blob_fixup_graphic_buffer_size),
+    ('vendor/lib64/hw/camera.qcom.so', 'vendor/lib64/libFaceDetectpp-0.5.2.so', 'vendor/lib64/libfacedet.so'): blob_fixup()
+        .binary_regex_replace(b'libmegface.so', b'libfacedet.so')
+        .binary_regex_replace(b'libMegviiFacepp-0.5.2.so', b'libFaceDetectpp-0.5.2.so')
+        .binary_regex_replace(b'megviifacepp_0_5_2_model', b'facedetectpp_0_5_2_model'),
     ('vendor/lib64/libalLDC.so', 'vendor/lib64/libalhLDC.so'): blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')
